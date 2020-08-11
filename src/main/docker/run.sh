fa3ab1277ab76d4f2c652b212f5d38ade12deb5b
mariamihai <maria.ic.mihai@gmail.com>
2020-08-11 23:30:51 +0200

    Update

--- src/main/docker/run.sh
+++
@@ -1,4 +0,0 @@
-echo "********************************************************"
-echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
-echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
