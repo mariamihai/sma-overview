be53c5a7b2fbf7b997ce293365bd4d757686a307
mariamihai <maria.ic.mihai@gmail.com>
2020-08-14 10:51:23 +0200

    Initial commit

---
+++ src/test/java/com/thoughtmechanix/smaeurekaservice/SmaEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.smaeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
