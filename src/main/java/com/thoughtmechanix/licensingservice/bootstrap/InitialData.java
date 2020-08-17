d7f62e16d3346aa059e3b4c09b329d2ddaea53f3
mariamihai <maria.ic.mihai@gmail.com>
2020-08-17 12:58:51 +0200

    Update InitialData class.

--- src/main/java/com/thoughtmechanix/licensingservice/bootstrap/InitialData.java
+++ src/main/java/com/thoughtmechanix/licensingservice/bootstrap/InitialData.java
@@ -25,7 +25,7 @@ public class InitialData implements CommandLineRunner {
         addLicense("89aaf748-85d8-4d03-9ef7-e97f5647cbf6", "user", "customer-crm-co", 100, 5);
         addLicense("89aaf748-85d8-4d03-9ef7-e97f5647cbf6", "user", "suitability-plus", 200, 189);
         addLicense("6f1b066a-17ee-45ca-8923-4f9538e7cde5", "user", "sHR-PowerSuite", 100, 4);
-        addLicense("b08f92eb-0787-4aee-b317-a8b40fb7cd8a", "core-prod", "WildCat Application Gateway", 16, 16);
+//        addLicense("b08f92eb-0787-4aee-b317-a8b40fb7cd8a", "core-prod", "WildCat Application Gateway", 16, 16);
     }
 
     private void addLicense(String organizationIdString, String licenseType, String productName,
@@ -36,7 +36,8 @@ public class InitialData implements CommandLineRunner {
                 .productName(productName)
                 .licenseMax(licenseMax)
                 .licenseAllocated(licenseAllocated)
-                .build();
+            .build();
+
         licenseRepository.save(license);
     }
 
