3026bcb25744f02b6c70a31329f5b0cb4748e8d2
mariamihai <maria.ic.mihai@gmail.com>
2020-08-19 22:31:55 +0200

    Adding Spring Cloud annotation for the Zuul service

--- src/main/java/com/thoughtmechanix/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/thoughtmechanix/gatewayservice/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.thoughtmechanix.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayServiceApplication {
 
