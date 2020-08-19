8ded009a5dc47906a99d42ddec0d192b4f5c8f33
mariamihai <maria.ic.mihai@gmail.com>
2020-08-19 22:02:12 +0200

    Initial commit.

---
+++ src/main/java/com/thoughtmechanix/gatewayservice/SmaGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmaGatewayServiceApplication.class, args);
+    }
+
+}
