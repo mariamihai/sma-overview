9eba6eb24faa8e5c735271e4947ed16ff2b60d1f
mariamihai <maria.ic.mihai@gmail.com>
2020-08-25 16:59:48 +0200

    Initial commit.

---
+++ src/main/java/com/thoughtmechanix/specialroutesservice/SpecialRoutesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.specialroutesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialRoutesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecialRoutesServiceApplication.class, args);
+    }
+
+}
