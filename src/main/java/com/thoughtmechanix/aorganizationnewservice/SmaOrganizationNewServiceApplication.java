b24275dfd3d27b5dfb8895f96314518735b1733d
mariamihai <maria.ic.mihai@gmail.com>
2020-08-26 13:50:08 +0200

    Initial commit.

---
+++ src/main/java/com/thoughtmechanix/aorganizationnewservice/SmaOrganizationNewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.aorganizationnewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaOrganizationNewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmaOrganizationNewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtmechanix/aorganizationnewservice/SmaOrganizationNewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.aorganizationnewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaOrganizationNewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
