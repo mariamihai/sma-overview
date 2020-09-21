d3eaf6c1c69cb74190b0361b26367a46855da5c9
mariamihai <maria.ic.mihai@gmail.com>
2020-09-21 15:14:49 +0200

    Updated controller.

--- src/main/java/com/thoughtmechanix/smasecurityservice/controllers/AuthenticationController.java
+++ src/main/java/com/thoughtmechanix/smasecurityservice/controllers/AuthenticationController.java
@@ -3,17 +3,15 @@ package com.thoughtmechanix.smasecurityservice.controllers;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "auth")
 public class AuthenticationController {
 
-    @GetMapping
+    @GetMapping(value = "/user")
     public Map<String, Object> user(OAuth2Authentication user) {
         Map<String, Object> userInfo = new HashMap<>();
         userInfo.put("user", user.getUserAuthentication().getPrincipal());
